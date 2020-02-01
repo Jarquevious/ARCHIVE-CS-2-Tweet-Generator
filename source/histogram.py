521607ed59eb500f55d22c246647777a60fa2feb
Jarquevious <jarquevious@gmail.com>
2020-01-31 20:50:08 -0800

    histogram working

--- source/histogram.py
+++ source/histogram.py
@@ -13,42 +13,68 @@
 # What is the average (mean/median/mode) frequency of words in the text?
 
 
-filename = "adamsmith.txt"
-lines = open(filename, "r")
-word_histogram = {}
+# filename = "adamsmith.txt"
+# lines = open(filename, "r")
+# word_histogram = {}
 
-def histogram():
+# '''Create function that takes a source text as params and creates a dictionary representing a histogram of words in a list'''
+sample_string = 'run love May'
+
+def histogram(sample_string):
   word_histogram = {}
-
-  for word in lines:
-  
-   word = word.rstrip()
+  words = sample_string.split()
+  for word in words:
+    print(word)
   #TODO: add code to increase the count in the histogram for the given word
-  if word not in word_histogram:
-    word_histogram[word]=1
-  else:
-      word_count_value = word_histogram.get(word)
-      word_count_value += 1
-      word_histogram[word] = word_count_value
+    if word not in word_histogram:
+      word_histogram[word]=1
+    else:
+      word_histogram[word]+= 1
   return word_histogram
+
+def unique_words(word_histogram):
+  return len(word_histogram.keys())
+  
+def frequency(hist):
+  user_input = input('enter a word: ')
+  return hist[user_input]
+
+
+hist = histogram(sample_string)
+print(hist)
+print(unique_words(hist))
+print (frequency(hist))
+
+
       
 
-def unique_word():
-  num_of_keys = 0
-  keys_in_words_histogram = word_histogram.keys()
-  for i in keys_in_words_histogram:
-    num_of_keys =+ 1
-  return num_of_keys
-  print(num_of_keys)
+# def unique_word():
+#   num_of_keys = 0
+#   keys_in_words_histogram = word_histogram.keys()
+#   for i in keys_in_words_histogram:
+#     num_of_keys =+ 1
+#   return num_of_keys
+#   print(num_of_keys)
 
 
-def frequency():
-  user_input = input("enter a word")
+# def frequency():
+#   user_input = input("enter a word")
+
+
   
+# print(histogram())
+# # print(" ")
+
+# sample_string = ['run', 'love', 'May']
+# def histogram():
+#   word_dictionary = {}
+#   words = sample_string.split()
+
 
   
-print(histogram())
-# print(" ")
-
-
-
+  # for word in sample_string:
+  #   if word in word_dictionary:
+  #     word_dictionary[word] += 1
+  #   else:
+  #     word_dictionary[word] = 1
+  # for key in 
